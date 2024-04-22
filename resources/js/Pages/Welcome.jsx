b050c12bffd131a0c572c1cdfc955b8548f1ffa6
JuanMiguelElric <oliveirajuan243@gmail.com>
2024-04-22 12:00:59 -0300

    leo aqui pareceiro

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,7 +5,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
         <>
             <Head title="Eaeee" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <p>leo aqui</p>
+                <p>leo aqui meu amigo</p>
             </div>
 
             
